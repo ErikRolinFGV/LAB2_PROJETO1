283f9e8c8aef20c6b8bbf0dc5810a18382a7cf7e
ErikRolinFGV <erik7nonato@gmail.com>
2025-08-12 17:08:07 -0300

    import random salva muito

--- lista1.py
+++ lista1.py
@@ -1,7 +1,20 @@
-#Ola de novo Pestana, CUMprimento é mt forte
+#Ola de novo pestana, seria mt legal se voce realmente tivesse lendo isso e que não tenha problema ser de 1 a 100
+
+import random
 
 def cumprimento(texto):
     return f"Olá, {texto}"
 
+
+def media_numeros_aleatorios():
+    numeros = [random.randint(1, 100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    return media
+
+
 mensagem = cumprimento("Erik Nonato Rolin")
 print(mensagem)
+
+media = media_numeros_aleatorios()
+#print(media)
+print(f"A média dos 7 números sorteados é: {media:.2f}")
